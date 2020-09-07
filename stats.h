f1f390623f3a176e3b6d39dee5608e4bed4eef97
haneef15 <51503680+haneef15@users.noreply.github.com>
2020-09-07 11:13:29 +0530

    Update stats.h

--- stats.h
+++ stats.h
@@ -1,5 +1,13 @@
 #include <vector>
 
+struct Stats
+{
+    double average, min, max;
+    void ComputeAverage(const std::vector<double>&);
+    void ComputeMax(const std::vector<double>&);
+    void ComputeMin(const std::vector<double>&);
+};
+
 namespace Statistics {
-    Stats ComputeStatistics(const std::vector<___>& );
+    Stats ComputeStatistics(const std::vector<double>&);
 }
